2a7ac9d23624556f10e115b56b3c60807cf9205f
Laraib Saghir <saghirlaraib08@gmail.com>
2023-11-30 20:59:21 +0500

    chat position bottom

--- src/SuperAdmin/pages/Chat/ChatMessages.jsx
+++ src/SuperAdmin/pages/Chat/ChatMessages.jsx
@@ -1,30 +1,40 @@
 import { Text, Box, ScrollArea } from '@mantine/core';
+import { useRef, useLayoutEffect } from 'react';
 
 export default function ChatMessages({ messages }) {
+  const viewport = useRef(null);
+
+  const scrollToBottom = () =>
+    viewport.current.scrollTo({ top: viewport.current.scrollHeight, behavior: 'smooth' });
+
+  useLayoutEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
-    <ScrollArea h={400} type="never">
-    <Box>
-      {messages.map((message, index) => (
-        <Box key={index} align={message.status === 'send' ? 'right' : 'left'} >
-          <Box w={'50%'}
-            style={{
-              borderRadius: '10px', // Adjust the borderRadius as needed
-              overflow: 'hidden', // Ensure the borderRadius is applied
-            }}
-           >
-          <Text
-          bg={message.status === 'send' ? 'lime.9' : 'lime.1'}
-           //color={message.status === 'send' ? 'white' : 'black'}
-           style={{ textAlign: 'justify' }}
-           p={10}
-           >
-            {message.message}
-          </Text>
-          <Text size={'0.75rem'}>{message.createdAt}</Text>
+    <ScrollArea h={400} type="never" viewportRef={viewport}>
+      <Box>
+        {messages.map((message, index) => (
+          <Box key={index} align={message.status === 'send' ? 'right' : 'left'}>
+            <Box
+              w={'50%'}
+              style={{
+                borderRadius: '10px',
+                overflow: 'hidden',
+              }}
+            >
+              <Text
+                bg={message.status === 'send' ? 'lime.9' : 'lime.1'}
+                style={{ textAlign: 'justify' }}
+                p={10}
+              >
+                {message.message}
+              </Text>
+              <Text size={'0.75rem'}>{message.createdAt}</Text>
+            </Box>
           </Box>
-        </Box>
-      ))}
-    </Box>
+        ))}
+      </Box>
     </ScrollArea>
   );
 }
